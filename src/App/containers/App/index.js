dc448245ccd4e261afa1cee74c71a9a66414b88f
Venugopal <venugopala11000@gmail.com>
2019-01-20 23:50:53 +0530

    Added TAbs in Dashboard

--- src/App/containers/App/index.js
+++ src/App/containers/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '../Home/index';
 import Header from '../../components/header';
 import { Switch, Route } from 'react-router-dom';
+import Dashboard from '../Dashboard';
 
 export default class App extends React.Component {
     render() {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
             <Header />
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route path='/:userName' component={(params) => <Dashboard userName={params.match.params.userName}/>} />
                 {/* both /roster and /roster/:number begin with /roster */}
                 {/* <Route path='/roster' component={Roster} />
                     <Route path='/schedule' component={Schedule} /> */}
--- src/App/containers/Home/index.js
+++ src/App/containers/Home/index.js
@@ -4,17 +4,10 @@ import Dashboard from '../Dashboard';
 import './style.scss';
 
 export default class Home extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-    componentDidMount() {
-    }
     render() {
         return (
             <div className="">
-                {!localStorage.getItem("USER") ? <LogIn /> : <Dashboard />}
+                <LogIn />
             </div>
         );
     }
